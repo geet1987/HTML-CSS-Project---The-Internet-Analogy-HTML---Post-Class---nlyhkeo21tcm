a52d8ef18bde333ec10a474ba52378cbf9775c1f
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-12-14 00:01:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const contents = document.querySelectorAll('.content')
 const listItems = document.querySelectorAll('nav ul li')
 
 listItems.forEach((item,idx) => {
-    item.addEventListener('click',()=>{
+    item.addEventListener('click',() => {
         hideAllContents()
         hideAllItems()
 
